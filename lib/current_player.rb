f900dafe9d4bae9cc4a9014f5340a96d9e74ea1a
Harrison Chung <harrison.y.chung@gmail.com>
2020-09-02 17:12:56 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,20 @@
 #turn_count method
 def turn_count(board)
   turns = 0
-  board.each do |space_on_board|
-    if space_on_board == "X" || space_on_board == "O"
+  board.each do |space|
+    if space == "X" || space == "O"
       turns += 1
     end
   end
-  return
+  return turns
 end
 
-#currnet_player method
+#current_player method
 def current_player(board)
- turns = turn_count(board)
- if turn % 2 == O
-   return "X"
- else
-   return "O"
- end
- end
+  turns = turn_count(board)
+  if turns % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
